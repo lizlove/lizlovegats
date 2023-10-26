64c652aba4181134b382a0bb327e88f7b821e1dd
Elizabeth Lovero <7707627+lizlove@users.noreply.github.com>
2023-10-26 13:34:54 -0500

    Update config.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,10 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `lizlovegats`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Elizabeth Lovero`,
+    description: `Elizabeth Lovero is a developer and technical product manager based in Chicago & Brooklyn, where she builds data visualizations and periodically writes about development, design, and the detritus of digital life.`,
+    author: `github.com/lizlove`,
+    siteUrl: `https://www.elizabethlovero.com`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
