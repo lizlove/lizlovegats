f69156185b74e3ed924c390d72eb89949331bfab
Elizabeth Lovero <7707627+lizlove@users.noreply.github.com>
2023-11-03 15:02:44 -0500

    Cleanup!

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -2,10 +2,52 @@ import { ItemProps } from "../types/all-types";
 
 export const projects: ItemProps[] = [
   {
+    title: "Seeing Solitary",
+    date: "2022-2023",
+    description:
+      "The Seeing Solitary digital dashboard presents data about the scope and impact of solitary confinement in prisons across the United States and the world. A project of the Arthur Liman Center for Public Interest Law at Yale School, the data visualization dashboard is designed to help policymakers, advocates, journalists, and the public understand the use of solitary confinement in prisons and to inform efforts to reduce its use.",
+    link: "https://seeingsolitary.limancenter.yale.edu/",
+  },
+  {
+    title: "Chern-Simons Caps of Rotating Black Hole",
+    date: "2021",
+    description:
+      "This black hole visualization illustrates universal axion movement and the impact of Chern-Simons gravity in the work of the Flatiron Institute's IDEA Scholar in Residence, Dr. Stephon Alexander. Built in both Blender and Three.js, this visualization was featured in the 2021 Flatiron Institute Annual Report.",
+    link: "https://blackhole.vercel.app/",
+  },
+  {
+    title: "FlatHUB",
+    date: "2021",
+    description:
+      "Flatiron Institute Data Exploration and Comparison Hub (FlatHUB) enables researchers to easily explore and compare large astrophysics simulation and observation datasets. Users can filter on multiple relevant fields, generate figures, and export their filtered data in a variety of formats. This project aims to facilitate discourse on the presented research by offering a web-based platform for exploration and analysis of datasets that are normally too massive for easy sharing and manipulation.",
+    link: "https://flathub.flatironinstitute.org/",
+  },
+  {
     title: "SpikeForest",
     date: "2019-2020",
     description:
       "SpikeForest is a website and open source computing framework for evaluating and comparing spike sorting algorithms for neurophysiology data analysis. The project is documented in a May 2020 eLife paper, linked below in the Writing section.",
     link: "http://spikeforest.flatironinstitute.org/",
+  },
+  {
+    title: "Donut Time",
+    date: "2019-2020",
+    description:
+      "Show gratitude to coworkers on a Slack workspace using custom donut emojis and slash commands. Built upon the Howdy.ai platform for a company hackathon at Haven Life, Donut Time also includes a leaderboard to track the most thanked and most thankful employees.",
+    link: "https://github.com/lizlove/donut-time-bot",
+  },
+  {
+    title: "Publicradio.info",
+    date: "2017",
+    description:
+      "Publicradio.info is a web app for listening to noncommercial news, culture, and art. The rugged anti-design was featured on Brutalist Websites",
+    link: "https://publicradio.info/",
+  },
+  {
+    title: "dB NYC",
+    date: "2015",
+    description:
+      "dB NYC is a visualization of the noise pollution in New York City. The app was created using Ruby on Rails and maps the location of 311 noise complaints, emergency services, social check-ins, and social venue reviews to create a dynamic heat-map of noise broken down over a 24 hour period.",
+    link: "https://github.com/lizlove/db-nyc",
   },
 ];
